d057990420de4c95013d9e7452b483213fb8e0a4
Marj <faustinomarjj@gmail.com>
2023-07-20 14:27:30 +0800

    feat: added home layout

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
